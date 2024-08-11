f08720d10039b7150b42a5a257b6f46ea1b97b1a
axtar <axtario@gmail.com>
2024-08-12 00:09:22 +0200

    Update Settings.h

--- Software/Firmware/Solar Monitor/include/Settings.h
+++ Software/Firmware/Solar Monitor/include/Settings.h
@@ -20,9 +20,13 @@
 // IP address of the inverter
 #define INVERTER_IPADDRESS "xxx.xxx.xxx.xxx"
 
-// the power values are approximations
-#define LOAD_CORRECTION 0 // in watts
-
 // values in the defined range are set to 0 to keep the display quieter
 // set this value to 0 if you dont want to round towards zero
 #define POWER_ROUND_TO_ZERO_RANGE 10 // in watts
+
+// methods to get the load power value
+#define LOADPOWER_CALCULATE 1    // calculate the load power using the grid, solar and battery power values
+#define LOADPOWER_FROMINVERTER 2 // take the load power value provided by the inverter, always slightly lower as calculated
+
+// used method to get the load power value
+#define GET_LOADPOWER_METHOD LOADPOWER_CALCULATE
